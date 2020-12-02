ce578c2342e9d00c593c4c81ccbf7f90ae6605e5
rsundone <pavel.serhiichuk@gmail.com>
2020-12-02 23:28:20 +0200

    37. Added store

--- src/index.js
+++ src/index.js
@@ -3,25 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-import state, {addPost, addMessage, updatePostText, updateDialogMessage, subscribe} from './redux/state'
+import store, {addPost, addMessage, updatePostText, updateDialogMessage, subscribe} from './redux/store'
 
-const rerender = () => {
+const rerender = (state) => {
     return ReactDOM.render(
     <BrowserRouter>
       <React.StrictMode>
         <App state={state}
-             addPost={addPost}
-             updatePostText={updatePostText}
-             addMessage={addMessage} updateDialogMessage={updateDialogMessage}/>
+             addPost={store.addPost.bind(store)}
+             updatePostText={store.updatePostText.bind(store)}
+             addMessage={store.addMessage.bind(store)}
+             updateDialogMessage={store.updateDialogMessage.bind(store)}
+        />
       </React.StrictMode>
     </BrowserRouter>
     ,
     document.getElementById('root')
 );}
 
-rerender()
+rerender(store.getState())
 
-subscribe(rerender)
+store.subscribe(rerender)
 
 export default rerender
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/store.js
@@ -0,0 +1,119 @@
+
+let rerender = () => {
+    console.log('rerender')
+}
+
+const store = {
+    subscribe(observer) {
+        this._rerender = observer
+    },
+    _rerender() {
+        console.log('rerender')
+    },
+    _state: {
+        profileInfo: {
+            posts: [
+                {id: 1, message: 'Glad to see everyone', likes: 0},
+                {id: 2, message: 'Glad to see you', likes: 3},
+                {id: 3, message: 'Glad to see everyone', likes: 5},
+            ],
+            postText: 'gord soboy',
+        },
+        dialogInfo: {
+            users: [
+                {id: 1, name: 'Elena'},
+                {id: 2, name: 'Igor'},
+                {id: 3, name: 'Paul'}
+            ],
+            messages: [
+                { id: 1, message:"Hello"},
+                { id: 2, message:"How are you?"},
+                { id: 3, message:"I'm excellent"},
+            ],
+            messageText: 'gord soboy'
+        },
+    },
+    getState() {
+        return this._state
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            likes: 0,
+            message: this._state.profileInfo.postText
+        }
+        this._state.profileInfo.posts.unshift(newPost)
+        this._rerender(this._state)
+    },
+    updatePostText(newPostText) {
+        this._state.profileInfo.postText = newPostText
+        this._rerender(this._state)
+
+    },
+    addMessage() {
+        this._state.dialogInfo.messages.push({id: 4, message: store.dialogInfo.messageText})
+        this._rerender(this._state)
+
+    },
+    updateDialogMessage(newMessageText) {
+        this._state.dialogInfo.messageText = newMessageText
+        this._rerender(this._state)
+
+    },
+}
+
+// const store = {
+//     profileInfo: {
+//         posts: [
+//             {id: 1, message: 'Glad to see everyone', likes: 0},
+//             {id: 2, message: 'Glad to see you', likes: 3},
+//             {id: 3, message: 'Glad to see everyone', likes: 5},
+//         ],
+//         postText: 'gord soboy',
+//     },
+//     dialogInfo: {
+//         users: [
+//             {id: 1, name: 'Elena'},
+//             {id: 2, name: 'Igor'},
+//             {id: 3, name: 'Paul'}
+//         ],
+//         messages: [
+//             { id: 1, message:"Hello"},
+//             { id: 2, message:"How are you?"},
+//             { id: 3, message:"I'm excellent"},
+//         ],
+//         messageText: 'gord soboy'
+//     },
+// }
+
+export const updatePostText = (newPostText) => {
+    store.profileInfo.postText = newPostText
+    rerender()
+
+}
+
+export const updateDialogMessage = (newMessageText) => {
+    store.dialogInfo.messageText = newMessageText
+    rerender()
+}
+
+export const addPost = () => {
+    let newPost = {
+        id: 5,
+        likes: 0,
+        message: store.profileInfo.postText
+    }
+    store.profileInfo.posts.unshift(newPost)
+    rerender()
+}
+
+export const addMessage = () => {
+    store.dialogInfo.messages.push({id: 4, message: store.dialogInfo.messageText})
+    rerender()
+}
+
+export const subscribe = (observer) => {
+    rerender = observer //adder rerender from index.js
+}
+
+export default store
